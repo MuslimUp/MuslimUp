f82131a932d3fd4a9530937875629533e74a02a2
MuslimUp <benhassen.anissa099@gmail.com>
2025-10-22 10:23:14 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AuthTest from "./AuthTest";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthTest />
+    <App />
   </React.StrictMode>
 );
